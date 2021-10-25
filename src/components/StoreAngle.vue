4e14ebddca48e3635856319a8b53348c4ce84f1a
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2021-10-25 11:05:47 +0900

    Fix: scriptタグの順番など

--- src/components/StoreAngle.vue
+++ src/components/StoreAngle.vue
@@ -1,14 +1,13 @@
-<template>
-  <div class="StoreAngleView">
-    <input v-model.number="angle" type="text" />{{ unit }}
-  </div>
-</template>
-
 <script setup lang="ts">
 import { toRef } from 'vue';
 import { useStoreAngle } from '../hooks/useStoreAngle';
+
 const props = defineProps<{
   unit: 'deg' | 'rad';
 }>();
 const angle = useStoreAngle(toRef(props, 'unit'));
 </script>
+
+<template>
+  <div><input v-model.number="angle" type="text" />{{ unit }}</div>
+</template>
--- src/components/StoreLength.vue
+++ src/components/StoreLength.vue
@@ -1,14 +1,13 @@
-<template>
-  <div class="StoreLengthView">
-    <input v-model.number="length" type="text" />{{ unit }}
-  </div>
-</template>
-
 <script setup lang="ts">
 import { toRef } from 'vue';
 import { useStoreLength } from '../hooks/useStoreLength';
+
 const props = defineProps<{
   unit: 'px' | 'inch' | 'mm';
 }>();
 const length = useStoreLength(toRef(props, 'unit'));
 </script>
+
+<template>
+  <div><input v-model.number="length" type="text" />{{ unit }}</div>
+</template>
