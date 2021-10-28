14bf9b59c3c32fb45a67a317166849592672497a
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2021-10-28 15:39:37 +0900

    Fix: Topなど

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -5,15 +5,20 @@
     <h1>Vue3プロジェクトテンプレート</h1>
     <div>
       <h3>参考文献,引用元</h3>
-      <p>
-        ICS MEDIAさま<a href="https://ics.media/entry/210929/"
-          >コンポーネントを小さく・きれいに設計しよう Vue Composition
-          APIを活用したコンポーネント分割術</a
-        >
-      </p>
-      <p>
-        <a href="https://pinia.esm.dev/introduction.html">Pinia</a>
-      </p>
+
+      <ul>
+        <li>
+          ICS MEDIA
+          <a href="https://ics.media/entry/210929/"
+            >コンポーネントを小さく・きれいに設計しよう Vue Composition
+            APIを活用したコンポーネント分割術</a
+          >
+        </li>
+        <li><a href="https://pinia.esm.dev/introduction.html">Pinia</a></li>
+        <li>
+          <a href="https://vueuse.org/core/usestorage/">VueUse useStorage</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
