dceeac340c72027e4bc95913d2c17924e341c75c
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2022-01-07 19:33:34 +0900

    ADD:node16系に変更, aliasの設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
   build: {
     assetsDir: 'assets',
     outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@/': path.join(__dirname, './src/'),
+    },
   },
 });
